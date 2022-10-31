bad81ce4a12ecd07c2a49107f1189487d7f7c3b8
Владимир Трунов <vetrunov@chsu.ru>
2022-10-31 11:15:54 +0300

    add new entery point

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+export type {
+    IADAuthAdress,
+    IADUsersAdress,
+    IADAuth,
+    IADSettings,
+    AppAuthResponse,
+    IUserAD,
+
+} from './src';
+
+export {UserAD} from './src';
+export {Authorization} from './src';
